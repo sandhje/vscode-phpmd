e738b9f1227fcb18d513180715aaebd961849b8b
Quentin Dreyer <quentin.dreyer@gmail.com>
2019-09-10 09:35:19 +0200

    feat: expand rules with os homedir

--- server/service/pipeline/ExecuteProcessStrategy.ts
+++ server/service/pipeline/ExecuteProcessStrategy.ts
@@ -1,5 +1,6 @@
 import { IExecuteStrategy } from "@open-sourcerers/j-stillery";
 import * as Process from "child_process";
+import { homedir } from "os";
 import PipelineErrorModel from "../../model/PipelineErrorModel";
 import PipelinePayloadModel from "../../model/PipelinePayloadModel";
 import ILogger from "../logger/ILogger";
@@ -101,7 +102,7 @@ class ExecuteProcessStrategy implements IExecuteStrategy<PipelinePayloadModel> {
      * @returns {Promise<string>}
      */
     protected executeProcess(path: string): Promise<string> {
-        return this.getService().run("\"" + path + "\"" + " xml \"" + this.rules + "\"");
+        return this.getService().run("\"" + path + "\"" + " xml \"" + this.rules.replace(/^~/, homedir()) + "\"");
     }
 }
 
